1105554fe9b307722338eb868a46cf71dc9f20a7
C453 <Caseman912@me.com>
2015-05-14 21:44:36 -0400

    Redesign

---
+++ Font Installer/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace Font_Installer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        [DllImport("gdi32.dll")]
+        static extern int AddFontResource(string file);
+        private static readonly List<string> Fonts = new List<string>();
+
+        private uint _errorCount;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_OnDrop(object sender, DragEventArgs e)
+        {
+	        if (!e.Data.GetDataPresent(DataFormats.FileDrop)) return;
+
+	        var files = (string[]) e.Data.GetData(DataFormats.FileDrop);
+
+	        foreach(var font in files.Where(i => i.EndsWith(".ttf") || i.EndsWith(".otf") || i.EndsWith(".fnt")))
+	        {
+		        listBox.Items.Add(System.IO.Path.GetFileName(font));
+		        Fonts.Add(font);
+	        }
+        }
+
+        private void ImportFontsBtn_Click(object sender, RoutedEventArgs e)
+        {
+	        if (Fonts.Count == 0) return;
+
+            Parallel.ForEach(Fonts, j =>
+            {
+                try
+                {
+                    AddFontResource(j);
+                }
+                catch
+                {
+                    _errorCount++;
+                }
+            });
+
+            if (_errorCount > 0)
+            {
+                MessageBox.Show(string.Format("There were some errors installing fonts. {0} out of {1} fonts have been installed.", Fonts.Count - _errorCount, Fonts.Count));
+                return;
+            }
+
+            MessageBox.Show("All fonts installed successfully\nTheese fonts will be avaliable until shutdown.");
+        }
+    }
+}
--- Font Installer/Properties/Resources.Designer.cs
+++ Font Installer/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Font_Installer.Properties {
-    using System;
-    
-    
+namespace Font_Installer.Properties
+{
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,60 +22,49 @@ namespace Font_Installer.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
-        
+    internal class Resources
+    {
+
         private static global::System.Resources.ResourceManager resourceMan;
-        
+
         private static global::System.Globalization.CultureInfo resourceCulture;
-        
+
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources() {
+        internal Resources()
+        {
         }
-        
+
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
-            get {
-                if (object.ReferenceEquals(resourceMan, null)) {
+        internal static global::System.Resources.ResourceManager ResourceManager
+        {
+            get
+            {
+                if ((resourceMan == null))
+                {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Font_Installer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-        
+
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
-            get {
+        internal static global::System.Globalization.CultureInfo Culture
+        {
+            get
+            {
                 return resourceCulture;
             }
-            set {
+            set
+            {
                 resourceCulture = value;
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Linux_penguin_computing {
-            get {
-                object obj = ResourceManager.GetObject("Linux_penguin_computing", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
-        /// </summary>
-        internal static System.IO.UnmanagedMemoryStream OH_GOD_AHHH {
-            get {
-                return ResourceManager.GetStream("OH_GOD_AHHH", resourceCulture);
             }
         }
     }
